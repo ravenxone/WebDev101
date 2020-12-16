568201cf1cb5a15ae1388e6f06fc9e5d1e67e0a5
unsortedArray <ricks.rv5@gmail.com>
2020-12-16 23:00:25 +0530

    added js101

---
+++ JS101/index.js
@@ -0,0 +1,116 @@
+// console.log("Damn! Hello world! from another JS page!")
+
+// I am a single line comment
+
+/*
+    I am a multi line comment
+*/
+
+var a = 42;
+var b = "I am a string";
+var c = true;
+// prints are by default as strings
+
+console.log(a+b); // 42I am a string
+console.log(a+c); // 42true
+
+console.log(b+c);//I am string true
+
+// Maths operator
+
+var a = 2;
+var b = 3;
+var c = 4;
+var d=5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/2);
+console.log(a%b);
+
+console.log(a++);
+console.log(a--);
+
+a +=4;
+console.log(a);
+a=42;
+var anotherA = "42";
+var justTrue ="true";
+var deepTrue = true;
+// equality
+
+//shallow equality 
+
+console.log(a == anotherA);
+console.log(justTrue == deepTrue);
+
+// deep equality 
+
+console.log(justTrue === deepTrue);
+
+console.log("Here we go!")
+// maths equality
+a =3;
+b=2;
+c=5;
+d=7;
+
+console.log(a<b);
+console.log(c<=d);
+
+// logical operations
+
+console.log( a<b && c <d); 
+
+
+// values which are permissable in js for now we will talk about only numbers, strings and booleans
+
+
+// coercion
+
+var e = true;
+var f = false;
+
+console.log(a + e); // implicit coercion
+
+// i.e a= 3 and e = true e -> true =1 by js internally and thus ans will be 4
+
+console.log(a+"some string");
+
+// i.e here a = number and some string is some string and thus we will see that number is converted into string
+
+// ans = 3some string
+
+// explicit coercion
+
+console.log(a + Number(e));
+
+console.log(String(a)+ "some string");
+
+// here we have made changes accordingly and thus we know what to do! always prefer explicit coercion over impl
+// cause implicit js coercion gives weird answers sometimes
+
+// static and dyanamic typing
+
+var someVariable =2;
+console.log(someVariable);
+someVariable = "some variable";
+console.log(someVariable);
+
+someVariable  = true;
+console.log(someVariable);
+
+
+
+// Let's talk about some constants in native js
+
+var PERCENTAGE_OF_COOL_KIDS_I_HAVE = 100;
+
+// blocks
+
+if(PERCENTAGE_OF_COOL_KIDS_I_HAVE == 100) {
+    console.log("Damn Array! Quite a batch you have!");
+}else{
+    console.log("How could you be so heartless!")
+}
